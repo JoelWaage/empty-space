e981e0907577c3e6930e89ee1ebbcfad70f5de60
Joel <joeltwaage@gmailcom>
2016-09-07 13:12:57 -0700

    add shopping cart funtionality

--- app/routes/script.js
+++ app/routes/script.js
@@ -3,5 +3,13 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model(params) {
     return this.store.findRecord('script', params.script_id);
+  },
+
+  shoppingCart: Ember.inject.service(),
+
+  actions: {
+    addToCart(item) {
+      this.get('shoppingCart').add(item);
+    },
   }
 });
