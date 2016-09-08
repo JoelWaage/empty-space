64cb7fde92f9d5311628e5e9c8d409c85c0886ab
Joel <joeltwaage@gmail.com>
2016-09-08 11:30:08 -0700

    add ability to add and display added amount on checkout page

--- app/services/shopping-cart.js
+++ app/services/shopping-cart.js
@@ -2,8 +2,11 @@ import Ember from 'ember';
 
 export default Ember.Service.extend({
   items: [],
+  total: 0,
 
   add(item) {
     this.get('items').pushObject(item);
-  }
+    var number = (parseInt(this.get('total')) + parseInt(item.get('price')))
+    this.set('total', number);
+  },
 });
